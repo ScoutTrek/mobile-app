5d99813289916b25c2a60ecb9a07029eb3abf8a6
Kaleb Davenport <kaleb@theearlynerd.org>
2019-12-19 15:45:16 -0500

    Configured helpers

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   bracketSpacing: false,
+  semi: false,
+  tabWidth: 2,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
-};
+}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,7 @@
-import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
+// @ flow
+
+import * as WebBrowser from 'expo-web-browser'
+import React from 'react'
 import {
   Image,
   Platform,
@@ -8,9 +10,9 @@ import {
   Text,
   TouchableOpacity,
   View,
-} from 'react-native';
+} from 'react-native'
 
-import { MonoText } from '../components/StyledText';
+import {MonoText} from '../components/StyledText'
 
 export default function HomeScreen() {
   return (
@@ -43,35 +45,14 @@ export default function HomeScreen() {
             Change this text and your app will automatically reload.
           </Text>
         </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
       </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
     </View>
-  );
+  )
 }
 
 HomeScreen.navigationOptions = {
   header: null,
-};
+}
 
 function DevelopmentModeNotice() {
   if (__DEV__) {
@@ -79,33 +60,33 @@ function DevelopmentModeNotice() {
       <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
         Learn more
       </Text>
-    );
+    )
 
     return (
       <Text style={styles.developmentModeText}>
         Development mode is enabled: your app will be slower but you can use
         useful development tools. {learnMoreButton}
       </Text>
-    );
+    )
   } else {
     return (
       <Text style={styles.developmentModeText}>
         You are not in development mode: your app will run at full speed.
       </Text>
-    );
+    )
   }
 }
 
 function handleLearnMorePress() {
   WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
+    'https://docs.expo.io/versions/latest/workflow/development-mode/',
+  )
 }
 
 function handleHelpPress() {
   WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
+    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes',
+  )
 }
 
 const styles = StyleSheet.create({
@@ -156,43 +137,4 @@ const styles = StyleSheet.create({
     lineHeight: 24,
     textAlign: 'center',
   },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
+})
