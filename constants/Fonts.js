8884535dd71eba0ec5e92f02bf4b2177db70f97a
Kaleb Davenport <kaleb@theearlynerd.org>
2022-02-18 09:00:28 -0500

    Remove constants folder including fonts

--- constants/Fonts.js
+++
@@ -1,7 +0,0 @@
-export default {
-  headingBold: 'raleway-bold',
-  headingBlack: 'raleway-black',
-  primaryText: 'montserrat',
-  primaryTextBold: 'montserrat-bold',
-  primaryTextLight: 'montserrat-light',
-};
